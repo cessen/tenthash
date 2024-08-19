975d2a1adf67ffc5b72953afd71b3af66106f2b0
Nathan Vegdahl <cessen@cessen.com>
2024-08-19 23:01:36 +0200

    Slightly more interesting hash collision example.

--- supplemental/collision/src/main.rs
+++ supplemental/collision/src/main.rs
@@ -1,44 +1,72 @@
 //! A program that runs TentHash in reverse to intentionally create hash
-//! collisions.
+//! collisions with specific target hash outputs.
 //!
-//! The purpose of this is mainly for fun, but also to clearly demonstrate that
-//! TentHash shouldn't be used under adversarial circumstances.  It is not a
+//! The purpose of this is magnly for fun, but also to clearly demonstrate that
+//! TentHash shouldn't be used under adversarial conditions.  It is not a
 //! cryptographic hash.
 
-fn main() {
-    let collisions = generate_collisions([0u8; 20], 10);
+use nanorand::{Rng, WyRand};
 
-    println!("Here are ten pieces of data that each produce a hash of all zeros:");
+fn main() {
+    let target_hash = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    ];
+
+    let collisions = generate_colliding_messages(target_hash, 10);
+
+    println!("Here are ten pieces of data that all produce the same hash:");
     for data in &collisions {
         println!("    {:?}", data);
     }
 
-    println!("And here are the corresponding hashes, to verify:");
+    println!("\nAnd here are the corresponding hashes, to verify:");
     for data in &collisions {
-        println!("    {:?}", hash(data));
+        println!("    {:?}", tenthash::hash(data));
     }
 }
 
-fn hash(data: &[u8]) -> [u8; 20] {
-    let mut hasher = tenthash::TentHasher::new();
-    hasher.update(data);
-    hasher.finalize()
-}
-
-/// Generates `count` messages that when hashed by TentHash produce the given target output.
-fn generate_collisions(target: [u8; 20], count: usize) -> Vec<Vec<u8>> {
+/// Generates `count` messages that when hashed by TentHash produce the given
+/// target output.
+fn generate_colliding_messages(target: [u8; 20], count: usize) -> Vec<Vec<u8>> {
+    let mut rng = WyRand::new();
     let mut colliding_data = Vec::new();
-    for i in 0..count {
+
+    // You can set this to whatever size you like, for longer or shorter
+    // messages.
+    let payload_size_in_blocks = 1;
+
+    for _ in 0..count {
+        let mut data = Vec::new();
+
         let mut state = [
             u64::from_le_bytes((&target[0..8]).try_into().unwrap()),
             u64::from_le_bytes((&target[8..16]).try_into().unwrap()),
             u32::from_le_bytes((&target[16..20]).try_into().unwrap()) as u64,
-            i as u64,
+            rng.generate::<u64>(),
         ];
 
         unmix_state(&mut state);
         unmix_state(&mut state);
-        state[0] ^= 256;
+        state[0] ^= 256 * (1 + payload_size_in_blocks);
+
+        for _ in 0..payload_size_in_blocks {
+            unmix_state(&mut state);
+
+            // This can be any data.  In this case, we randomly generate it.
+            let data0 = rng.generate::<u64>();
+            let data1 = rng.generate::<u64>();
+            let data2 = rng.generate::<u64>();
+            let data3 = rng.generate::<u64>();
+
+            state[0] ^= data0;
+            state[1] ^= data1;
+            state[2] ^= data2;
+            state[3] ^= data3;
+            data.extend_from_slice(&data3.to_be_bytes());
+            data.extend_from_slice(&data2.to_be_bytes());
+            data.extend_from_slice(&data1.to_be_bytes());
+            data.extend_from_slice(&data0.to_be_bytes());
+        }
 
         unmix_state(&mut state);
         state[0] ^= 0x5d6daffc4411a967;
@@ -46,11 +74,12 @@ fn generate_collisions(target: [u8; 20], count: usize) -> Vec<Vec<u8>> {
         state[2] ^= 0xca50864d814cbc2e;
         state[3] ^= 0x894e29b9611eb173;
 
-        let mut data = Vec::new();
-        data.extend_from_slice(&state[0].to_le_bytes());
-        data.extend_from_slice(&state[1].to_le_bytes());
-        data.extend_from_slice(&state[2].to_le_bytes());
-        data.extend_from_slice(&state[3].to_le_bytes());
+        data.extend_from_slice(&state[3].to_be_bytes());
+        data.extend_from_slice(&state[2].to_be_bytes());
+        data.extend_from_slice(&state[1].to_be_bytes());
+        data.extend_from_slice(&state[0].to_be_bytes());
+
+        data.reverse();
 
         colliding_data.push(data);
     }
@@ -125,11 +154,11 @@ mod test {
     }
 
     #[test]
-    fn generate_collisions_01() {
-        let h = hash(b"Hello world!");
-        let cols = generate_collisions(h, 20);
+    fn generate_colliding_messages_01() {
+        let h = tenthash::hash(b"Hello world!");
+        let cols = generate_colliding_messages(h, 20);
         for data in cols {
-            let h2 = hash(&data);
+            let h2 = tenthash::hash(&data);
             assert_eq!(h, h2);
         }
     }
